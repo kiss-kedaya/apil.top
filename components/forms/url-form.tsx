7b80e6dccd3564018e10dd062571720aca2c64cf
可达鸭 <3443833799@qq.com>
2025-05-01 07:05:19 +0800

    a

--- components/forms/url-form.tsx
+++ components/forms/url-form.tsx
@@ -236,14 +236,22 @@ export function UrlForm({
                   </SelectTrigger>
                   <SelectContent>
                     {isLoadingDomains ? (
-                      <SelectItem value={siteConfig.shortDomains[0]}>
-                        <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
-                        Loading...
-                      </SelectItem>
+                      <div className="flex flex-col space-y-1 px-1 py-2">
+                        <div className="flex items-center space-x-2">
+                          <div className="h-4 w-4 animate-spin rounded-full border-2 border-primary border-t-transparent"></div>
+                          <div className="h-4 w-20 animate-pulse rounded-md bg-muted"></div>
+                        </div>
+                        <div className="flex items-center px-2">
+                          <div className="h-5 w-full animate-pulse rounded-md bg-muted"></div>
+                        </div>
+                        <div className="flex items-center px-2">
+                          <div className="h-5 w-full animate-pulse rounded-md bg-muted"></div>
+                        </div>
+                      </div>
                     ) : (
-                      availableDomains.map((v) => (
-                        <SelectItem key={v} value={v}>
-                          {v}
+                      availableDomains.map((domain) => (
+                        <SelectItem key={domain} value={domain}>
+                          {domain}
                         </SelectItem>
                       ))
                     )}
